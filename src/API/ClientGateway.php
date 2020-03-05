474bc8006f61ec6447e4f0e2a0003832ef54e34a
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-05 14:21:15 +0300

    Add ClientGateway Interface

--- src/API/ClientGateway.php
+++ src/API/ClientGateway.php
@@ -4,5 +4,9 @@ namespace TarfinLabs\Parasut\API;
 
 interface ClientGateway
 {
+    public function getAccessToken(): string;
+
+    public function getRefreshToken(): string;
+
     public function authenticate(): bool;
 }
