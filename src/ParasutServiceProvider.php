b3e92fdbd8fbfad16c2409178828e8de7e85f232
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-05 14:22:56 +0300

    ParasutServiceProvider: Merge configs in `register()` method

--- src/ParasutServiceProvider.php
+++ src/ParasutServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace TarfinLabs\Parasut;
 
-use TarfinLabs\Parasut\API\HttpClientGateway;
-use Illuminate\Support\ServiceProvider;
 use TarfinLabs\Parasut\Entities\Contact;
 use TarfinLabs\Parasut\API\ClientGateway;
+use TarfinLabs\Parasut\API\HttpClientGateway;
+use Illuminate\Support\ServiceProvider;
 
 class ParasutServiceProvider extends ServiceProvider
 {
@@ -14,8 +14,6 @@ class ParasutServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->mergeConfigs();
-
         if ($this->app->runningInConsole()) {
             $this->publishConfigs();
         }
@@ -26,17 +24,17 @@ class ParasutServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->mergeConfigs();
+
         // Register the main class to use with the facade
-        $this->app->singleton(ClientGateway::class, function () {
-            return new HttpClientGateway(
-                config('parasut.grant_type'),
-                config('parasut.client_id'),
-                config('parasut.client_secret'),
-                config('parasut.username'),
-                config('parasut.password'),
-                config('parasut.redirect_uri'),
-            );
-        });
+        $this->app->singleton(ClientGateway::class, fn() => new HttpClientGateway(
+            config('parasut.grant_type'),
+            config('parasut.client_id'),
+            config('parasut.client_secret'),
+            config('parasut.username'),
+            config('parasut.password'),
+            config('parasut.redirect_uri'),
+        ));
     }
 
     protected function mergeConfigs(): void
