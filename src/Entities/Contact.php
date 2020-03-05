e8ea98387a51af605ec7704cd46eaef4d648ae91
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-05 20:06:01 +0300

    Add `Contact` Entity

--- src/Entities/Contact.php
+++ src/Entities/Contact.php
@@ -6,24 +6,90 @@ class Contact extends BaseEntitiy
 {
     protected string $endpoint = 'contacts';
 
-    protected array $availableSorts = [
-        'id',
-        'balance',
-        'name',
-        'email',
-    ];
+    // region Sorts
 
-    protected array $availableFilters = [
-        'name',
-        'email',
-        'tax_number',
-        'tax_office',
-        'city',
-    ];
+    public function sortById(bool $descending = false): Contact
+    {
+        return $this->sortByAttribute('id', $descending);
+    }
 
-    protected array $availableIncludes = [
-        'category',
-        'contact_portal',
-        'contact_people',
-    ];
+    public function sortByBalance(bool $descending = false): Contact
+    {
+        return $this->sortByAttribute('balance', $descending);
+    }
+
+    public function sortByName(bool $descending = false): Contact
+    {
+        return $this->sortByAttribute('name', $descending);
+    }
+
+    public function sortByEmail(bool $descending = false): Contact
+    {
+        return $this->sortByAttribute('email', $descending);
+    }
+
+    // endregion
+
+    // region Filters
+
+    public function findByName(string $name): Contact
+    {
+        $this->filters['name'] = $name;
+
+        return $this;
+    }
+
+    public function findByEmail(string $email): Contact{
+        $this->filters['email'] = $email;
+
+        return $this;
+    }
+
+    public function findByTaxNumber(string $taxNumber): Contact
+    {
+        $this->filters['tax_number'] = $taxNumber;
+
+        return $this;
+    }
+
+    public function findByTaxOffice(string $taxOffice): Contact
+    {
+        $this->filters['tax_office'] = $taxOffice;
+
+        return $this;
+    }
+
+    public function findByCity(string $city): Contact
+    {
+        $this->filters['city'] = $city;
+
+        return $this;
+    }
+
+    // endregion
+
+    // region Includes
+
+    public function includeCategory(): Contact
+    {
+        $this->includes[] = 'category';
+
+        return $this;
+    }
+
+    public function includeContactPortal(): Contact
+    {
+        $this->includes[] = 'contact_portal';
+
+        return $this;
+    }
+
+    public function includeContactPeople(): Contact
+    {
+        $this->includes[] = 'contact_people';
+
+        return $this;
+    }
+
+    // endregion
 }
