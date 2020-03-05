ff4a8fc0a9c93ef2d089434747b1c2eb21d24fdd
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-05 20:07:08 +0300

    BaseEntitiy: Add `all()` method

--- src/Entities/BaseEntitiy.php
+++ src/Entities/BaseEntitiy.php
@@ -3,18 +3,37 @@
 namespace TarfinLabs\Parasut\Entities;
 
 use TarfinLabs\Parasut\API\ClientGateway;
+use TarfinLabs\Parasut\Enums\HttpMethods;
 
 abstract class BaseEntitiy
 {
     protected ClientGateway $clientGateway;
-    protected string $endpoint;
-    protected array $availableSorts;
-    protected array $availableIncludes;
-    protected array $availableFilters;
 
-    public function __construct()
+    protected string $endpoint;
+
+    protected array $sorts = [];
+    protected array $filters = [];
+    protected array $includes = [];
+    protected int $page = 1;
+    protected int $pageSize = 25;
+
+    public function __construct(ClientGateway $clientGateway)
     {
-        $this->clientGateway = app(ClientGateway::class);
+        $this->clientGateway = $clientGateway;
+    }
+
+    public function all()
+    {
+        return $this->clientGateway->call(
+            HttpMethods::GET,
+            $this->endpoint,
+            $this->filters,
+            $this->sorts,
+            $this->includes,
+            null,
+            $this->page,
+            $this->pageSize
+        );
     }
 
 }
