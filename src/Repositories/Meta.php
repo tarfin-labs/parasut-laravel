5db00e5d41f32d8fc0e437f23f9aad702077e968
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-09 13:28:43 +0300

    Add `Meta` class for repositories

---
+++ src/Repositories/Meta.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TarfinLabs\Parasut\Repositories;
+
+class Meta
+{
+    public int    $currentPage;
+    public int    $totalPages;
+    public int    $totalCount;
+    public int    $perPage;
+    public string $exportUrl;
+
+    // TODO: Look if other models has `payable_total` and `collectible_total` attributes
+
+    public function __construct(array $meta)
+    {
+        $this->currentPage = $meta['current_page'];
+        $this->totalPages = $meta['total_pages'];
+        $this->totalCount = $meta['total_count'];
+        $this->perPage = $meta['per_page'];
+        $this->exportUrl = $meta['export_url'];
+    }
+}
