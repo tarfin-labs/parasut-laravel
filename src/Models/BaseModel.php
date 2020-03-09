c812a648a5c22f06c7ba98b76815a5ff02198ea1
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-09 15:07:21 +0300

    BaseModel: Fill `$fillables` using `$schema`

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class BaseModel extends Model
 {
     use Sushi;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->fillable = array_keys($this->schema);
+    }
 }
