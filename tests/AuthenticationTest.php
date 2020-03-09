4bce824c3af2b6a5c8f653e9c1165a3a50a3c914
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-09 15:24:35 +0300

    AuthenticationTest: Use `ParasutMock::authentication()` and simplify test

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Carbon;
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Http;
 use TarfinLabs\Parasut\API\ClientGateway;
+use TarfinLabs\Parasut\Mocks\ParasutMock;
 use TarfinLabs\Parasut\ParasutServiceProvider;
 
 class AuthenticationTest extends TestCase
@@ -19,19 +20,7 @@ class AuthenticationTest extends TestCase
     /** @test */
     public function user_can_authenticate(): void
     {
-        Http::fake([
-            config('parasut.api_url').'*' => Http::response([
-                'access_token'  => 'fake-access-token',
-                'token_type'    => 'bearer',
-                'expires_in'    => 7200,
-                'refresh_token' => 'fake-refresh-token',
-                'scope'         => 'public',
-                'created_at'    => 1583243989,
-                'created_at'    => Carbon::now()->unix(),
-            ], 200, [
-                'content-type' => 'application/json; charset=utf-8',
-            ]),
-        ]);
+        ParasutMock::authentication();
 
         $parasutClient = app(ClientGateway::class);
 
