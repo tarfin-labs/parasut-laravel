1da23c216de9a5e20016887a74042b0ebf828be9
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-10 16:26:35 +0300

    ContactTest: Add `user_can_create_a_new_contact()` test

--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -2,6 +2,7 @@
 
 namespace TarfinLabs\Parasut\Tests;
 
+use Faker\Factory;
 use TarfinLabs\Parasut\Models\Contact;
 use TarfinLabs\Parasut\Tests\Mocks\ParasutMock;
 use TarfinLabs\Parasut\Repositories\ContactRepository;
@@ -24,6 +25,26 @@ class ContactTest extends TestCase
     /** @test */
     public function user_can_create_a_new_contact(): void
     {
-        $this->assertTrue(true);
+        $faker = Factory::create('tr_TR');
+
+        $contact = new Contact();
+        $contact->name = '#TESTX# '.$faker->name;
+        $contact->account_type = $faker->randomElement(['customer', 'supplier']);
+
+        ParasutMock::createContact($contact);
+
+        $contactRepository = new ContactRepository();
+
+        $contactReturned = $contactRepository->create($contact);
+
+        $this->assertInstanceOf(
+            Contact::class,
+            $contact
+        );
+
+        $this->assertEquals(
+            $contact->name,
+            $contactReturned->name
+        );
     }
 }
