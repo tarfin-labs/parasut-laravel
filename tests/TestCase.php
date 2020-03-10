25a7bc4c27c64fb8c15df21a82e1f611ea520c11
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-10 18:10:51 +0300

    TestCase: Create a faker instance for all tests on `setUp()` method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,16 +2,20 @@
 
 namespace TarfinLabs\Parasut\Tests;
 
+use Faker\Factory;
+use Faker\Generator;
 use Orchestra\Testbench\TestCase as Orchestra;
 use TarfinLabs\Parasut\ParasutServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
+    protected Generator $faker;
+
     public function setUp(): void
     {
         parent::setUp();
 
-        //
+        $this->faker = Factory::create('tr_TR');
     }
 
     protected function getPackageProviders($app): array
