5619b7d9ed000886998a9c73925bf726529a035f
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-12 19:14:51 +0300

    ProductTest: Add `user_can_list_products()` test

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -4,5 +4,16 @@ namespace TarfinLabs\Parasut\Tests;
 
 class ProductTest extends TestCase
 {
-    
+    /** @test */
+    public function user_can_list_products(): void
+    {
+        ProductMock::all();
+
+        $productRepository = new ProductRepository();
+
+        $products = $productRepository->all();
+
+        $this->assertNotNull(Product::all());
+        $this->assertInstanceOf(Product::class, $products->first());
+    }
 }
