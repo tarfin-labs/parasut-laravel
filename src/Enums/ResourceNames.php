a02b1e120fa43311c54fba52211d6f55cef9c561
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-13 00:58:05 +0300

    ResourceNames: Add `buildEndpoint()` method

--- src/Enums/ResourceNames.php
+++ src/Enums/ResourceNames.php
@@ -6,4 +6,30 @@ class ResourceNames
 {
     public const CONTACT = 'contacts';
     public const PRODUCT = 'products';
+
+    /**
+     * Build the endpoint string from array or string parts.
+     *
+     * @param  mixed  ...$endpoints
+     *
+     * @return string
+     */
+    public static function buildEndpoint(...$endpoints): string
+    {
+        $url = '';
+
+        foreach ($endpoints as $endpoint) {
+            if (is_string($endpoint))
+            {
+                $url .= "{$endpoint}/";
+            }
+
+            if (is_array($endpoint))
+            {
+                $url .= implode('/', $endpoint);
+            }
+        }
+
+        return $url;
+    }
 }
